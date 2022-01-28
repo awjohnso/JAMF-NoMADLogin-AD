50d1278381ce0c33169d9dd2f702415ab8271d67
Andrew W Johnson <Andrew.Johnson@stonybrook.edu>
2022-01-28 09:24:33 -0500

    A few more updates.
    Added echoing to the local log file: /var/log/jamf.log

--- DSE-DisableNoMADLogin-AD.zsh
+++ DSE-DisableNoMADLogin-AD.zsh
@@ -13,16 +13,19 @@ if [[ -e "/usr/local/bin/authchanger" ]]; then
 		# Check on the status and see if NoMADLogin-AD is set.
 	myNoMAD=$(/usr/local/bin/authchanger -Print | /usr/bin/egrep -ic NoMADLoginAD)
 	if [[ ${myNoMAD} -ge 1 ]]; then
-		/bin/echo "NoMADLogin-AD is functioning. Disabling NoMadLogin-AD"
+		/bin/echo "NoMADLogin-AD is enabled. Disabling NoMADLogin-AD"
+		/bin/echo "$( /bin/date | /usr/bin/awk '{print $1, $2, $3, $4}' ) $( /usr/sbin/scutil --get LocalHostName ) $( /usr/bin/basename ${0} )[$$]: NoMADLogin-AD is enabled. Disabling NoMADLogin-AD" >> /var/log/jamf.log
 			# Disable NoMADLogin-AD.
         /usr/local/bin/authchanger -reset
 #		/bin/launchctl kickstart -k system/com.apple.loginwindow
 		/sbin/reboot
 	else
 		/bin/echo "NoMADLogin-AD is not enabled."
+		/bin/echo "$( /bin/date | /usr/bin/awk '{print $1, $2, $3, $4}' ) $( /usr/sbin/scutil --get LocalHostName ) $( /usr/bin/basename ${0} )[$$]: NoMADLogin-AD is not enabled." >> /var/log/jamf.log
 	fi
 else
 	/bin/echo "/usr/local/bin/authchanger does not seem to be installed."
+	/bin/echo "$( /bin/date | /usr/bin/awk '{print $1, $2, $3, $4}' ) $( /usr/sbin/scutil --get LocalHostName ) $( /usr/bin/basename ${0} )[$$]: /usr/local/bin/authchanger does not seem to be installed." >> /var/log/jamf.log
 fi
 
 exit 0
--- DSE-EnableNoMADLogin-AD.zsh
+++ DSE-EnableNoMADLogin-AD.zsh
@@ -15,9 +15,11 @@ if [[ -e "/usr/local/bin/authchanger" ]]; then
 		# Check on the status and see if NoMADLogin-AD is set.
 	myNoMAD=$(/usr/local/bin/authchanger -Print | /usr/bin/egrep -ic NoMADLoginAD)
 	if [[ ${myNoMAD} -ge 1 ]]; then
-		/bin/echo "NoMADLogin-AD is functioning."
+		/bin/echo "NoMADLogin-AD is enabled."
+		/bin/echo "$( /bin/date | /usr/bin/awk '{print $1, $2, $3, $4}' ) $( /usr/sbin/scutil --get LocalHostName ) $( /usr/bin/basename ${0} )[$$]: NoMADLogin-AD is enabled." >> /var/log/jamf.log
 	else
-		/bin/echo "NoMADLogin-AD is not working."
+		/bin/echo "NoMADLogin-AD is disabled."
+		/bin/echo "$( /bin/date | /usr/bin/awk '{print $1, $2, $3, $4}' ) $( /usr/sbin/scutil --get LocalHostName ) $( /usr/bin/basename ${0} )[$$]: NoMADLogin-AD is disabled." >> /var/log/jamf.log
 			# Enable NoMADLogin-AD.
 		/usr/local/bin/authchanger -reset -AD
 #		/bin/launchctl kickstart -k system/com.apple.loginwindow
@@ -25,5 +27,6 @@ if [[ -e "/usr/local/bin/authchanger" ]]; then
 	fi
 else
 	/bin/echo "/usr/local/bin/authchanger does not seem to be installed."
+	/bin/echo "$( /bin/date | /usr/bin/awk '{print $1, $2, $3, $4}' ) $( /usr/sbin/scutil --get LocalHostName ) $( /usr/bin/basename ${0} )[$$]: /usr/local/bin/authchanger does not seem to be installed." >> /var/log/jamf.log
 fi
 exit 0
